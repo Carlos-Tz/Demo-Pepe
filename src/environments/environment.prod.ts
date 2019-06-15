3f8817403f46f9c4d5026c3d19ec2eb1eb23fc2d
rdz <rdz.carlos@mail.com>
2019-06-15 15:22:31 -0500

    version 1.2.3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyBFMBQ-cq2OZuVZdbzpcMUWLvg0niWSTLs",
-    authDomain: "pepe-gaucho-a2c5d.firebaseapp.com",
-    databaseURL: "https://pepe-gaucho-a2c5d.firebaseio.com",
-    projectId: "pepe-gaucho-a2c5d",
-    storageBucket: "pepe-gaucho-a2c5d.appspot.com",
-    messagingSenderId: "227255126681"
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyBFMBQ-cq2OZuVZdbzpcMUWLvg0niWSTLs",
-    authDomain: "pepe-gaucho-a2c5d.firebaseapp.com",
-    databaseURL: "https://pepe-gaucho-a2c5d.firebaseio.com",
-    projectId: "pepe-gaucho-a2c5d",
-    storageBucket: "pepe-gaucho-a2c5d.appspot.com",
-    messagingSenderId: "227255126681"
-  }
+  production: false
 };
 
 /*
